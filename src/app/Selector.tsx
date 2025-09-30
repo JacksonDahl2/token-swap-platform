9db0d45b96afd4e14240a923040b2413dfa135d3
Jackson <jacksondahl27@gmail.com>
2025-09-30 13:26:40 -0400

    Config setup and basic platform created

---
+++ src/app/Selector.tsx
@@ -0,0 +1,12 @@
+
+const Home = async () => {
+
+  return (
+    <div>
+      <h3 className="text-primary-foreground text-2xl font-bold text-center mb-4">Token Price Explorer</h3>
+      <p className="text-primary-foreground/80 text-center">Welcome to your token swap platform</p>
+    </div>
+  )
+}
+
+export default Home;
